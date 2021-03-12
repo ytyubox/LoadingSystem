3952aa731d395e461d26aa4a0468c342881dabbf
Tsungyu Yu <ytyubox@gmail.com>
2021-03-12 18:07:11 +0800

    add Universal Presenter as only one display func

---
+++ Sources/LoadingSystem/Presenter/UniversalPresenter.swift
@@ -0,0 +1,61 @@
+//
+/* 
+ *		Created by 游宗諭 in 2021/3/12
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 11.2
+ */
+
+
+import Foundation
+
+public protocol UniversalView {
+    associatedtype Union
+
+    func display(_ model: Union)
+}
+
+open class UniversalPresenter<View: UniversalView, Input, Output> where View.Union == Output {
+   
+    
+    private let view: View
+    private let loadingTransformer: (Input) -> Output
+    private let successTransformer: (Input, Data) throws -> Output
+    private let successCaseFailureTransformer: ((Input, Error) -> Output)?
+    private let failureTransformer: (Input, Error)  -> Output
+
+    public init(view: View,
+                  LoadingTransformer: @escaping (Input)  -> Output,
+                  SuccessTransformer: @escaping (Input, Data) throws -> Output,
+                  successCaseFailureTransformer: ((Input, Error) -> Output)? = nil,
+                  FailureTransformer: @escaping (Input, Error)  -> Output) {
+        self.view = view
+        self.loadingTransformer = LoadingTransformer
+        self.successTransformer = SuccessTransformer
+        self.successCaseFailureTransformer = successCaseFailureTransformer
+        self.failureTransformer = FailureTransformer
+    }
+    
+    public func didStartLoadingImageData(for model: Input) {
+        let loading = loadingTransformer(model)
+        view.display(loading)
+    }
+
+    public func didFinishLoadingImageData(with data: Data, for model: Input) {
+        do {
+            let success = try successTransformer(model, data)
+            view.display(success)
+        } catch {
+            if let fromSuccessCase = successCaseFailureTransformer?(model, error) {
+                view.display(fromSuccessCase)
+            }
+        }
+        
+    }
+
+    public func didFinishLoadingImageData(with error: Error, for model: Input) {
+        let failure = failureTransformer(model, error)
+        view.display(failure)
+    }
+}
